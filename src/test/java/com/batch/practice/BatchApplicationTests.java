3d88da0175d49d8471f6744315d73310022759dc
kanghonggu <wckhg89@gmail.com>
2017-03-17 08:58:53 +0900

    set executionCtx to test code and understanding intercept through jobExcetion

--- src/test/java/com/batch/practice/BatchApplicationTests.java
+++ src/test/java/com/batch/practice/BatchApplicationTests.java
@@ -7,10 +7,16 @@ import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.BeforeStep;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.MetaDataInstanceFactory;
+import org.springframework.batch.test.StepScopeTestExecutionListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import java.util.List;
 
@@ -18,6 +24,8 @@ import static org.junit.Assert.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class,
+        StepScopeTestExecutionListener.class })
 @ContextConfiguration(classes = {BatchConfig.class, TestConfig.class})
 public class BatchApplicationTests {
 
@@ -33,6 +41,16 @@ public class BatchApplicationTests {
 	public void testStep1() throws Exception {
 		commonAssertions(jobLauncherTestUtils.launchStep("step1"));
 	}
+
+
+    @Test
+    public void testStep2() throws Exception {
+        StepExecution execution = MetaDataInstanceFactory.createStepExecution();
+        execution.getExecutionContext().putString("step2", "Pass To Next Step");
+
+        commonAssertions(jobLauncherTestUtils.launchStep("step2", execution.getExecutionContext()));
+    }
+
 
 	private void commonAssertions(JobExecution jobExecution) {
 		assertNotNull(jobExecution);
